2b37ffd9b7899f4cc8d8cd809c39658665434203
Davi Caetano <davicaetano@gmail.com>
2015-10-31 18:24:03 -0700

    Commit do efeito de sombra meio complicado com arquivos drawable .xml

---
+++ app/src/main/java/com/davicaetano/runningwithmusic/MainActivity.java
@@ -0,0 +1,20 @@
+package com.davicaetano.runningwithmusic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle("Teste");
+        setSupportActionBar(toolbar);
+
+    }
+}
