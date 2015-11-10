a6056a75e251db69082f5e99c435af575988c0e6
Davi Caetano <davicaetano@gmail.com>
2015-11-10 02:41:50 -0800

    Commit: 1- Data Contract; 2- Image files; 3- Layout with image files; 4- Using cardview.

--- app/src/main/java/com/davicaetano/runningwithmusic/MainActivity.java
+++ app/src/main/java/com/davicaetano/runningwithmusic/MainActivity.java
@@ -1,7 +1,7 @@
 package com.davicaetano.runningwithmusic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
-        toolbar.setTitle("Teste");
+        toolbar.setTitle("Runnig with music");
         setSupportActionBar(toolbar);
 
     }
---
+++ app/src/main/java/com/davicaetano/runningwithmusic/data/RWMContract.java
@@ -0,0 +1,26 @@
+package com.davicaetano.runningwithmusic.data;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class RWMContract {
+    public static final String CONTENT_AUTHORITY = "com.davicaetano.runningwithmusic";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    public static final class Songs implements BaseColumns{
+
+        public static final String TABLE_NAME = "songs";
+        public static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" +
+                CONTENT_AUTHORITY + "/" + TABLE_NAME;
+        public static final String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" +
+                CONTENT_AUTHORITY + "/" + TABLE_NAME;
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath("").build();
+
+        public static final String COLUMN_ARTIST = "artist";
+        public static final String COLUMN_SONG_NAME = "song_name";
+        public static final String COLUMN_ALBUM_NAME = "album_name";
+        
+    }
+}
